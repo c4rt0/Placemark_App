08a6e6d27ef22c65d7a58578bcaf0892fa0f55f6
c4rt0 <c4rt0gr4ph3r@gmail.com>
2020-12-28 22:07:48 +0000

    Implemented initial simple Menu

--- src/main/kotlin/org/wit/placemark/main.kt
+++ src/main/kotlin/org/wit/placemark/main.kt
@@ -4,7 +4,25 @@ import mu.KotlinLogging
 
 private val logger = KotlinLogging.logger {}
 
-fun main(args: Array<String>){
-    println("Placemark Kotlin App Version 1.0")
+fun main(args: Array<String>) {
     logger.info { "Launching Placemark Console App" }
+    println("Placemark Kotlin App Version 1.0")
+
+    var input: Int
+    input = menu()
+}
+
+fun menu() : Int {
+
+    var option : Int
+    var input: String? = null
+
+    println("Main Menu")
+    println(" 1. Add Placemark")
+    println("-1. Exit")
+    println()
+    print("Enter an integer : ")
+    input = readLine()!!
+    option = input.toInt()
+    return option
 }
