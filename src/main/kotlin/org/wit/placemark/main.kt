f1467954f089a54ec0cd3fcb10553f8a52362690
c4rt0 <c4rt0gr4ph3r@gmail.com>
2020-12-28 20:05:45 +0000

    Initial commit for the simple Kotlin app with logging

---
+++ src/main/kotlin/org/wit/placemark/main.kt
@@ -0,0 +1,10 @@
+package org.wit.placemark
+
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+
+fun main(args: Array<String>){
+    println("Placemark Kotlin App Version 1.0")
+    logger.info { "Launching Placemark Console App" }
+}
